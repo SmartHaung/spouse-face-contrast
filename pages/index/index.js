144cff66e1ded451541c9ca311eb3653676ede9e
SmartHaung <464510690@qq.com>
2017-12-25 11:06:11 +0800

    修改0分时候的提示

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,7 +10,9 @@ Page({
   },
   addPhoto(index) {
     var that = this;
-    that.setData({ score: 0 });
+    that.setData({
+      score: 0
+    });
     wx.chooseImage({
       success: function (res) {
         var tempFilePaths = res.tempFilePaths
@@ -29,10 +31,13 @@ Page({
           },
           success: function (res) {
             if (index == 1) {
-              that.setData({ url1: "http://huangwenbin.xin:8080/" + picName });
-            }
-            else {
-              that.setData({ url2: "http://huangwenbin.xin:8080/" + picName });
+              that.setData({
+                url1: "http://huangwenbin.xin:8080/" + picName
+              });
+            } else {
+              that.setData({
+                url2: "http://huangwenbin.xin:8080/" + picName
+              });
             }
           }
         })
@@ -60,20 +65,22 @@ Page({
       url: 'https://www.huangwenbin.xin/interface/face/contrast?url1=' + that.data.url1 + "&url2=" + that.data.url2,
       success: function (res) {
         if (res && res.data && res.data.response && res.data.response.code && res.data.response.code == 1001) {
-            wx.navigateTo({
-              url: '/pages/result/result?score=' + res.data.response.faceContrastResult.score
-            })
-        }
-        else {
-          wx.showToast({
-            title: "测试失败",
-            duration: 2000
+          wx.navigateTo({
+            url: '/pages/result/result?score=' + res.data.response.faceContrastResult.score
           })
+        } else {
+          wx.navigateTo({
+            url: '/pages/result/result?score=-1'
+          })
+          // wx.showToast({
+          //   title: "测试失败",
+          //   duration: 2000
+          // })
         }
-    },
-    complete: function() {
+      },
+      complete: function () {
         wx.hideLoading();
-    }
+      }
     })
   }
 })
